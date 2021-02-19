04649b32b68eebe75a9ecb98f432c6d5740a61cc
incora-dev <dev@incorainc.com>
2021-02-19 21:40:50 +0200

    Setup SCSS, prop-types, ESLint, prettier, router

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <p className="title">Home</p>
+    </div>
+  );
+};
+
+export default Home;
