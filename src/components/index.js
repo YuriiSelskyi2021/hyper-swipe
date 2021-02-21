eb545d4914116827151bfdec0f600ec78fd4ba0a
incora-dev <dev@incorainc.com>
2021-02-21 14:24:52 +0200

    Added fonts, Title, Paragraph components, Home page

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Paragraph from 'components/basic/Paragraph';
+import Title from 'components/basic/Title';
+
+export { Paragraph, Title };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Paragraph, Title } from 'components';
 import './index.scss';
 
 const Home = () => {
   return (
     <div className="home">
-      <p className="title">Home</p>
+      <Title Size="h3" className="top-text">
+        Welcome to the
+      </Title>
+      <Title Size="h1" className="home_title">
+        Hyper Swipe
+      </Title>
+      <Paragraph className="home_paragraph">
+        These are som things we like and maybe you like them to? Swipe your way through the stack
+        and sort the pile into things you like and dislike.
+      </Paragraph>
     </div>
   );
 };
